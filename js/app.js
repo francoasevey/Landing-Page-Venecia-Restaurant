22942c9966c7692e8d7280875bf5deeb9fe1deb5
francoromeo <francoromeo5012@gmail.com>
2024-04-03 23:39:45 -0300

    menu

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
 $(document).ready(function ($) {
   "use strict";
 
-  WebGLSampler.registerPlugin(ScrollTrigger);
+  jQuery(".menu-toggle").click(function() {
+    jQuery(".main-navigation").toggleClass("toggled");
+  });
+
+  jQuery(".header-menu ul li a").click(function() {
+    jQuery(".main-navigation").removeClass("toggled");
+  });
+
+  gsap.registerPlugin(ScrollTrigger);
 
   const elementFirst = document.querySelector('.site-header');
   ScrollTrigger.create({
