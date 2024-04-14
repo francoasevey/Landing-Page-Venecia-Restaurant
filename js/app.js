e38eabf7b35b1e57705d5ff473e577c8c5f20a1d
francoromeo <francoromeo5012@gmail.com>
2024-04-14 20:15:22 -0300

    team css

--- js/app.js
+++ js/app.js
@@ -29,6 +29,39 @@ $(document).ready(function ($) {
     },
   });
 
+  let teamSlider = new Swiper(".team-slider", {
+    slidesPerView : 3,
+    spaceBetween : 30,
+    loop : true,
+    autoplay : {
+      delay : 3000,
+      disableOnInteraction : false,
+    },
+    speed: 2000,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el : ".swiper-pagination",
+      clickable : true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1.2,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      992: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 3,
+      },
+    }
+  });
+
   jQuery(".filters").on("click", function () {
     jQuery("#menu-dish").removeClass("bydefault_show");
 });
